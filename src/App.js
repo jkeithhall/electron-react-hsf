286bd0f4c98bd7976cb536c4aad853b928c3d191
Eric Mehiel <emehiel@calpoly.edu>
2023-12-14 14:23:38 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import logo from './SimLab_sqblk.png';
 import './App.css';
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
+import './App.scss'
 
-function App() {
+function HSFNav() {
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+      <p>Catagories</p>
+      <li><button id="scenarioNav">Scenario</button></li>
+      <li>Tasks</li>
+      <li>System Model</li>
+      <li>Dependencies</li>
+      <li>Constraints</li>
+      <li>Simulate</li>
+      <li>Analyze</li>
+    </nav>
+  );
+}
+
+function HSFCard() {
+  return (
+      <Accordion flush>
+        <Accordion.Item eventKey='0'>
+          <Accordion.Header>HSF Scenario</Accordion.Header>
+            <Accordion.Body>
+              Some HSF Scenario Information Here!
+              <Button>A New Button
+              </Button>
+            </Accordion.Body>
+          </Accordion.Item>
+      </Accordion>
+  )
+}
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="grid-container">
+        <header className="App-header">
+          <h1>HSF Builder - PICASSO</h1>
+        </header>
+          <HSFNav />
+
+          <div class="work-space">
+              <HSFCard />
+          </div>
+          <aside>
+              <ul>
+                <li>
+                  <p><strong>Info Here</strong></p>
+                </li>
+                <li>
+                  <p><strong>Developed by:</strong></p>
+                  <p>Sim Master</p>
+                </li>
+              </ul>
+            </aside>
+            <footer>
+              <p> Copyright 2023 - Poly Sim</p>
+            </footer> 
+      </div>
     </div>
   );
 }
 
-export default App;
+//export default App;
+
